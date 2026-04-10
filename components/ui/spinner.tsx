4000146b75b7e2bd54c9855d5c608ec7fc9fcf22
Anghelo Alva <ance.anghelo@gmail.com>
2026-04-10 17:49:38 -0400

    Fix childen import in spinner

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,10 +1,20 @@
-import { cn } from "@/lib/utils"
-import { RiLoaderLine } from "@remixicon/react"
+import { cn } from "@/lib/utils";
+import { RiLoaderLine } from "@remixicon/react";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  children,
+  ...props
+}: React.ComponentProps<"svg">) {
   return (
-    <RiLoaderLine role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
+    <RiLoaderLine
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  );
 }
 
-export { Spinner }
+export { Spinner };
